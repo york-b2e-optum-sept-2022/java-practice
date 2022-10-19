d64f8a7f930e072edb832cc1cfe261b0033430e7
adam-street <adam.street.dev@gmail.com>
2022-10-19 09:50:40 -0500

    FizzBuzz impl

--- src/main/java/net/yorksolutions/demo/DemoApplication.java
+++ src/main/java/net/yorksolutions/demo/DemoApplication.java
@@ -15,44 +15,53 @@ public class DemoApplication {
     public static void main(String[] args) {
 //        SpringApplication.run(DemoApplication.class, args);
 
-        Greeter greeter = new Greeter("adam");
-        Greeter greeter2 = new Greeter("bob");
-        Greeter greeter3 = new Greeter("scott");
+        for (int i = 0; i < 50; i++) {
+            System.out.println(
+                    FizzBuzz.staticPlay(i)
+            );
+        }
 
-        // TS
-        // console.log()
-        System.out.println(
-                greeter3.greet()
-        );
-
-        Greeter foo = new Greeter("joe");
-        Greeter bar = foo;
-        bar.name = "sally";
-
-        System.out.println(
-                foo.greet()
-        );
-        System.out.println(
-                bar.greet()
-        );
-
-        Greeter baz = new Greeter("alana");
-        Greeter baz2 = baz;
-        baz2 = null;
-
-        System.out.println(
-                baz.greet()
-        );
-
-
-        Greeter foo1 = new Greeter("alana");
-        Greeter foo2 = new Greeter("abigail");
-        Greeter foo3 = foo1;
-        foo3 = foo2;
-
-        System.out.println(
-                foo3.greet()
-        );
     }
+
+
+//        Greeter greeter = new Greeter("adam");
+//        Greeter greeter2 = new Greeter("bob");
+//        Greeter greeter3 = new Greeter("scott");
+//
+//        // TS
+//        // console.log()
+//        System.out.println(
+//                greeter3.greet()
+//        );
+//
+//        Greeter foo = new Greeter("joe");
+//        Greeter bar = foo;
+//        bar.name = "sally";
+//
+//        System.out.println(
+//                foo.greet()
+//        );
+//        System.out.println(
+//                bar.greet()
+//        );
+//
+//        Greeter baz = new Greeter("alana");
+//        Greeter baz2 = baz;
+//        baz2 = null;
+//
+//        System.out.println(
+//                baz.greet()
+//        );
+//
+//
+//        Greeter foo1 = new Greeter("alana");
+//        Greeter foo2 = new Greeter("abigail");
+//        Greeter foo3 = foo1;
+//        foo3 = foo2;
+//
+//        System.out.println(
+//                foo3.greet()
+//        );
+//    }
 
 }
--- src/main/java/net/yorksolutions/demo/FizzBuzz.java
+++ src/main/java/net/yorksolutions/demo/FizzBuzz.java
@@ -7,6 +7,44 @@ If a number is divisible by both, they say “fizz buzz”
  */
 
 public class FizzBuzz {
+    int num;
+    static int foo = 50;
 
+    public FizzBuzz(int num) {
+        this.num = num;
+    }
 
+    public String play() {
+        boolean isD5 = this.num % 5 == 0;
+        boolean isD3 = this.num % 3 == 0;
+
+        if (isD5 && isD3) {
+            return "fizz buzz";
+        }
+        if (isD5) {
+            return "buzz";
+        }
+        if (isD3) {
+            return "fizz";
+        }
+
+        return Integer.toString(this.num);
+    }
+
+    public static String staticPlay(int num) {
+        boolean isD5 = num % 5 == 0;
+        boolean isD3 = num % 3 == 0;
+
+        if (isD5 && isD3) {
+            return "fizz buzz";
+        }
+        if (isD5) {
+            return "buzz";
+        }
+        if (isD3) {
+            return "fizz";
+        }
+
+        return Integer.toString(num);
+    }
 }
